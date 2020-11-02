7ebe1b317b9f4e8c46a59e9d08fb9bb06985b9b7
Bryan Cruz <bryan.oficio@gmail.com>
2020-11-02 02:11:52 -0300

    Primeiro componente

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-function App() {
+import Header from './components/Header/index';
+
+import './style.css';
+
+export default function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
-
-export default App;
